4d6c8cc3650e97c25f2ca826e8f9be55eeb990eb
princeallan <allankirui57@gmail.com>
2018-11-16 23:03:56 +0300

    Router Set and configured to new root directory home

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div>
+        <div class="title">
+            {{title}}
+        </div>
+    </div>
+
+</template>
+
+<script>
+/* eslint indent: "error" */
+
+export default {
+    data() {
+        return {
+            title: 'welcome to our vue App',
+        };
+    },
+};
+
+</script>
