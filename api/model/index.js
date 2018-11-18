28ebc60a4b150e289f9b6c2cc440477267c21bcd
princeallan <allankirui57@gmail.com>
2018-11-18 15:09:14 +0300

    model created. Manufacturer and product schema created

---
+++ api/model/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema,
+  model = mongoose.model.bind(mongoose),
+  ObjectId = mongoose.Schema.Types.ObjectId;
+
+const productSchema = Schema({
+  id: ObjectId,
+  name: String,
+  image: String,
+  price: Number,
+  description: String,
+  manufacturer: { type: ObjectId, ref: 'Manufacturer' },
+});
+
+const manufacturerSchema = Schema({
+  id: ObjectId,
+  name: String,
+});
